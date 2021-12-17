0d784ed383e60cde750c1eb48d5a5e9ec09f533c
frontend <fontend@lexos.com.br>
2021-12-17 20:41:10 -0300

    api cadaster

--- src/components/NewTransactionModa/index.tsx
+++ src/components/NewTransactionModa/index.tsx
@@ -3,7 +3,8 @@ import { Container, RadioBox, TransactionTypeContainer } from './styles';
 import closeSvg from '../../assets/close.svg';
 import icomeSvg from '../../assets/income.svg';
 import outcomeSvg from '../../assets/outcome.svg';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -13,7 +14,24 @@ interface NewTransactionModalProps {
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps){
     //Armazenando qual botao foi clicado
     //Storing which button was clicked
-    const [type, setType] = useState('deposit')
+    const [type, setType] = useState('deposit');
+
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState(0);
+    const [category, setCategory] = useState('');
+
+    function handleCreateNewTransaction(event: FormEvent) {
+        event.preventDefault();
+
+        const data = {
+            title,
+            value,
+            category,
+            type
+        }
+
+        api.post('/transactions', data)
+    }
 
 
     return (
@@ -26,11 +44,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             <button type="button" onClick={onRequestClose} className="react-modal-close">
                 <img src={closeSvg} alt="Close modal" />
             </button>
-            <Container>
+            <Container onSubmit={handleCreateNewTransaction}>
                 <h2>Cadastar transação</h2>
 
-                <input type="text" placeholder='Título' />
-                <input type="number" placeholder="Valor" />
+                <input 
+                    type="text" 
+                    placeholder='Título' 
+                    value={title} 
+                    onChange={event => setTitle(event.target.value)}
+                />
+                <input 
+                    type="number" 
+                    placeholder="Valor" 
+                    value={value} 
+                    onChange={event => setValue(Number(event.target.value))}
+                />
 
                 <TransactionTypeContainer>
                     <RadioBox 
@@ -53,7 +81,12 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     </RadioBox>
                 </TransactionTypeContainer>
 
-                <input type="text" placeholder="Categoria" />
+                <input 
+                    type="text" 
+                    placeholder="Categoria" 
+                    value={category} 
+                    onChange={event => setCategory(event.target.value)}
+                />
                 <button type="submit">Cadastar</button>
             </Container>
         </Modal>
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 
 createServer({
+  models: {
+    //Como se fosse uma tabela
+    transaction: Model,
+  },
+
+  //populated fake data base with some datas
+  //Populando o banco de dados fake com alguns dados
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 11:00:00')
+        },
+      ],
+    })
+  },
+
   routes() {
     //dizendo pro mirage que toda chamada vai estar a partir desse endereço aqui
     //All calls to API start with api
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id:  1,
-          title: 'Transaction 1',
-          amount: 400,
-          type:  'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction');
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
     })
   }
 })
