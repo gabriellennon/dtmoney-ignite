98c19d5b3aa3b4658821967f63d4a2d5da31645a
frontend <fontend@lexos.com.br>
2021-12-16 21:37:09 -0300

    modal content style

--- src/components/NewTransactionModa/index.tsx
+++ src/components/NewTransactionModa/index.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
+import closeSvg from '../../assets/close.svg';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,8 +12,20 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
-            <h2>Cadastar transação</h2>
+            <button type="button" onClick={onRequestClose} className="react-modal-close">
+                <img src={closeSvg} alt="Close modal" />
+            </button>
+            <Container>
+                <h2>Cadastar transação</h2>
+
+                <input type="text" placeholder='Título' />
+                <input type="number" placeholder="Valor" />
+                <input type="text" placeholder="Categoria" />
+                <button type="submit">Cadastar</button>
+            </Container>
         </Modal>
     );
 }
